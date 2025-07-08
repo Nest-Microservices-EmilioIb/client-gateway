82526aed5166253785d3dcafa88b04d29edb94f7
Emilio Ibarra <emilio.ibarra.270@gmail.com>
2025-07-08 08:38:55 -0600

    feat: order microservice connection added

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,78 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Inject,
+  ParseUUIDPipe,
+  Query,
+  Patch,
+} from '@nestjs/common';
+import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
+import { ORDER_SERVICE } from 'src/config';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
+  ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto || {});
+  }
+
+  @Get()
+  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    return this.ordersClient.send('findAllOrder', orderPaginationDto);
+  }
+
+  @Get(':status')
+  async findAllByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+      return await firstValueFrom(
+        this.ordersClient.send('findAllOrder', {
+          ...paginationDto,
+          status: statusDto.status,
+        }),
+      );
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Get('id/:id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    try {
+      return await firstValueFrom(
+        this.ordersClient.send('findOneOrder', { id }),
+      );
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Patch(':id')
+  async changeOrderStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: StatusDto,
+  ) {
+    try {
+      return await firstValueFrom(
+        this.ordersClient.send('changeOrderStatus', {
+          id,
+          status: statusDto.status,
+        }),
+      );
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ProductsController } from './products.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+
 import { envs, PRODUCT_SERVICE } from 'src/config';
+import { ProductsController } from './products.controller';
 
 @Module({
   controllers: [ProductsController],
-  providers: [],
   imports: [
     ClientsModule.register([
       {
